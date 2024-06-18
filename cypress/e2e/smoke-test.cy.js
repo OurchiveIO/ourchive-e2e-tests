4019651150139653c9365dc5e945b70a65fd29f8
c-e-p <c.elena.palmer@outlook.com>
2024-06-18 12:12:02 -0500

    Update smoke tests

--- cypress/e2e/smoke-test.cy.js
+++ cypress/e2e/smoke-test.cy.js
@@ -1,6 +1,9 @@
 describe('Anon User Browsing', () => {
-  it('visits the homepage and browses works', () => {
+  beforeEach(() => {
     cy.visit('http://127.0.0.1:8000/')
+  })
+
+  it('visits the homepage and browses works', () => {
 
     cy.get('[data-testid="browse-cards"]').first().click()
 
@@ -8,17 +11,43 @@ describe('Anon User Browsing', () => {
 
     cy.get('[data-testid="works-tab"]').invoke("text").should("contain", "Works")
 
-    cy.get('[data-testid="nav-works"]').click()
+    cy.get('[data-testid="ourchive-logo-link"]').click()
 
-    cy.url().should('include', '/works')
+    cy.get('[data-testid="browse-cards"]').first().click()
+
+    cy.url().should('include', '/search/?work_type')
 
     cy.get('[id$=-tile-card]').first().should('exist')
 
-    cy.get('[data-testid="nav-collections"]').click()
+    cy.visit('http://127.0.0.1:8000/username/1/')
 
-    cy.url().should('include', '/bookmark-collections')
+    cy.get('[id="user_collections_selector"]').click()
 
     cy.get('[id^=bkcol-tile-card-]').first().should('exist')
 
+    // series
+    cy.get('[id="user_series_selector"]').click()
+
+    cy.get('[data-testid="series-card-title"]').first().should('exist')
+
+    // anthologies
+
+    cy.get('[id="user_anthologies_selector"]').click()
+
+    cy.get('[data-testid="anthology-card-title"]').first().should('exist')
+
+  })
+
+  it('executes a search', () => {
+
+    cy.get('#nav-search-input')
+      .should('be.visible')
+      .type('untitled')
+
+    cy.get('[data-testid="navbar-search-submit"]')
+      .should('be.visible')
+      .click()
+
+    cy.get('[data-testid="work-tile-title"]').first().should('exist')
   })
 })
